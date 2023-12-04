ba0d035620c5105ed5ba958209390896a2d2815e
kwasicode <collinshicent@gmail.com>
2023-12-04 08:29:38 -0800

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,9 +3,8 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    """Print a matrix of integers."""
+    """Print a matrix."""
     for i in range(len(matrix)):
-        """Print a matrix of integers."""
         for j in range(len(matrix[i])):
                 print("{:d}".format(matrix[i][j]), end="")
                 if j != (len(matrix[i]) - 1):
