93a761447ed4ac416c9df2b1978354ced33d08df
kwasicode <collinshicent@gmail.com>
2023-12-04 08:21:33 -0800

    replace

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 2-replace_in_list.py
+# collins kwasi-effah
+
+
+def replace_in_list(my_list, idx, element):
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+    return (my_list)
